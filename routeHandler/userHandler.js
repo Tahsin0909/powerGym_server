185e0390ef78a27f64bdf4ee3bd41e261a035c77
Tahsin Zaman <tajisan789@gmail.com>
2024-10-04 21:57:10 +0600

    sfjdsjfwww

--- routeHandler/userHandler.js
+++ routeHandler/userHandler.js
@@ -9,7 +9,41 @@ const secret = process.env.ACCESS_TOKEN_SECRET;
 
 
 
+router.post('/', async (req, res) => {
+    const { name, email, password, role } = req.body; // Destructure name, email, password, and role from the request body
 
+    // Validate required fields
+    if (!name || !email || !password || !role) {
+        return res.status(400).json({ message: 'Name, email, password, and role are required' });
+    }
+
+    // Create a new user object
+    const user = new User({ name, email, password, role }); // Include role
+
+    // Hash the password before saving
+    try {
+        const salt = await bcrypt.genSalt(10); // Generate salt
+        user.password = await bcrypt.hash(password, salt); // Hash the password
+
+        const newUser = await user.save(); // Save the user to the database
+        // creating token and send to client
+        const userData = req.body
+        const token = jwt.sign(userData, secret, { expiresIn: '1h' })
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'none'
+        }).status(201).json({ success: true, statusCode: 201, message: 'User added successfully' });
+
+
+    } catch (err) {
+        // Handle duplicate email error
+        if (err.code === 11000) {
+            return res.status(400).json({ message: 'Email already exists' });
+        }
+        res.status(500).json({ message: err.message });
+    }
+});
 
 
 // get all users
